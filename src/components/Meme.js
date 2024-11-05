2198dc1c9f6aad8a6f243f88b5cdd80806469c35
Raghav Katta <Raghavkatta2005@gmail.com>
2024-11-06 01:52:15 +0530

    forms styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,26 @@
 import React from "react"
-export default function Meme(){
-    return(
+export default function Meme() {
+    return (
         <main>
-<form className="form">
-    <input type="text" className="form--input" placeholder="Top Text"/>
-    <input type="text" className="form--input" placeholder="Bottom Text"/>
-    <button>Get a meme image</button>
-</form>
+            <form className="form">
+                <div>
+                    <label>Top Text
+                        <input
+                            type="text"
+                            className="form--input"
+                            placeholder="Top Text"
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>Bottom Text
+                        <input
+                            type="text"
+                            className="form--input"
+                            placeholder="Bottom Text"
+                        /></label></div>
+                <button className="form--button">Get a meme image</button>
+            </form>
         </main>
     )
 }
